91cb0e41872241960c087ba6151dfbce6e36daa8
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2022-05-13 00:32:23 +0300

    Add files via upload

---
+++ Homework/Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picture;
+            int row = 3;
+            Console.WriteLine("Сколько у вас картин: ");
+            picture = Convert.ToInt32(Console.ReadLine());
+            int turnOut = picture / row;
+            int enumeration = picture % row;
+            Console.WriteLine($"Заполненных рядов: {turnOut}. Осталось картин: {enumeration}.");
+
+        }
+
+    }
+}
