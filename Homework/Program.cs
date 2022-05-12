97a3a8d46858fe88e9ebebb2384752b0e8985a49
balya <balya@DESKTOP-O5NT29T>
2022-05-12 21:50:05 +0300

    Добавьте файлы проекта.

---
+++ Homework/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coin;
+            int rubCoin;
+            int dollarCoin;
+            float cristal = 6.5f;
+            double redCristal = 2.5;
+            double blueCristal = 2.5;
+            char deaths = '3';
+            string life = "First dead";
+            bool Ifhedied = true;
+            bool Ifalive = false;
+        }
+
+    }
+}
