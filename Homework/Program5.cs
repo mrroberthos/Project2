c399f68067ae105d544f65f7301521d7ebc2cad5
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2022-05-13 17:54:57 +0300

    Update Program5.cs

--- Homework/Program5.cs
+++ Homework/Program5.cs
@@ -13,13 +13,11 @@ namespace Homework
             int oldladies;
             int receptionTime = 10;
             int waitingHour;
-            int waitingMinutes = 60;
+            int minutesPerHour = 60;
             Console.WriteLine("Введите количество старушек");
             oldladies = Convert.ToInt32(Console.ReadLine());
-            waitingHour = (oldladies * receptionTime) / waitingMinutes;
-            waitingMinutes = (oldladies * receptionTime) % waitingMinutes;
-            Console.WriteLine($"Вы должны отстоять в очереди {waitingHour} часа и {waitingMinutes} минут.");
-        }
+            waitingHour = (oldladies * receptionTime) / minutesPerHour;
+            Console.WriteLine($"Вы должны отстоять в очереди {waitingHour} часа и {waitingHour} минут.");
     }
 }
 
