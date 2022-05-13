0fc4e065e5d520d4e841dec39c53daae0abcb9f4
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2022-05-13 17:20:26 +0300

    Add files via upload

---
+++ Homework/Program5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oldladies;
+            int time = 10;
+            int hours;
+            int minutes;
+            Console.WriteLine("Введите количество старушек");
+            oldladies = Convert.ToInt32(Console.ReadLine());
+            hours = (oldladies * time) / 60;
+            minutes = (oldladies * time) % 60;
+            Console.WriteLine($"Вы должны отстоять в очереди {hours} часа и {minutes} минут.");
+        }
+    }
+}
+
