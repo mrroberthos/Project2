1cccd412ae2a1e830e993889c847c70a055ebf01
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2022-05-13 15:32:08 +0300

    Add files via upload

---
+++ Homework/Program3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "test1";
+            string surname = "test2";
+            string temp;
+            temp = name;
+            name = surname;
+            surname = temp;
+            Console.WriteLine(name + " " + surname);
+        }
+    }
+}
