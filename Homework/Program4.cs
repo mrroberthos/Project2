3c2cb344e223bad326e304278a64a0a3909465ea
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2022-05-13 16:35:59 +0300

    Add files via upload

---
+++ Homework/Program4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int cristallCount;
+            int cristallPrice = 20;
+            bool enoughGold;
+            Console.WriteLine($"Добро пожаловать в магазин кристаллов! Сегодня кристаллы по {cristallPrice} золота.");
+            Console.WriteLine("Сколько у вас золота?");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Сколько вам нужно кристаллов");
+            cristallCount = Convert.ToInt32(Console.ReadLine());
+            gold -= cristallCount * cristallPrice; 
+            Console.WriteLine($"У вас в сумке - {cristallCount} кристаллов и {gold} золота.");
+        }
+    }
+}
