6048d716bd0f6581eac966615a640a9e60a5897d
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2022-11-10 02:55:44 +0300

    Add files via upload

---
+++ Homework/Program7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Message;
+            Console.WriteLine("Введите своё сообщение:");
+            Message = Console.ReadLine();
+            int repeatsMessage;
+            Console.WriteLine("Введите кол-во повторений сообщения:");
+            repeatsMessage = Convert.ToInt32(Console.ReadLine());
+
+            while (repeatsMessage-- > 0)
+            {
+                Console.WriteLine(Message);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
