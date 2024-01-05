448f0c081a05a819df73258127395c0e2b686e57
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2024-01-05 22:52:33 +0300

    Add files via upload

---
+++ Homework/Program8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Первые_уроки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string closingProgram = "exit";
+            string userInput;
+ 
+            while (closingProgram == "exit") 
+            {
+                Console.WriteLine("Введите пароль или напишете exit");
+                userInput = Console.ReadLine();
+                if (userInput == closingProgram)
+                {
+                    Console.WriteLine("Пока");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Пробуй ещё");
+                }
+            }
+        }
+    }
+}
