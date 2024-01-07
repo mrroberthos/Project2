93353d5d2e2b347743a3f61bf2d38b9bb52cf4a5
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2024-01-07 20:49:24 +0300

    Add files via upload

---
+++ Homework/Program9.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Первые_уроки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i < 100; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
