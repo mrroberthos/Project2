51ae5f139ea7a3a53073d7707b769f912479cda2
mrroberthos <105454281+mrroberthos@users.noreply.github.com>
2024-01-09 16:10:58 +0300

    Add files via upload

---
+++ Homework/Program0.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Первые_уроки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coin;
+            float cristal = 6.5f;
+            double redCristal = 2.5;
+            char dead = '3';
+            string heal = "First dead";
+            bool Ifhedied = true; 
+            bool Ifalive = false;
+            byte cash = 100;
+            short memory = 32767;
+            long numberNPCs = 10000;
+
+        }
+    }
+}
